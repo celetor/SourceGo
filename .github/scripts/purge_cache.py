ff4c6c4b762a2aa4e895fd4add0564bdb10d4abb
Celeter <48249130+Celeter@users.noreply.github.com>
2021-02-28 12:16:34 +0800

    Update purge_cache.py

--- .github/scripts/purge_cache.py
+++ .github/scripts/purge_cache.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     if os.path.exists(source_path):
         for file in get_file_list(source_path):
             #print(file)
-            url = f'https://cdn.jsdelivr.net/gh/Celeter/SourceGo@main/book_source/{file}'
+            url = f'https://cdn.jsdelivr.net/gh/Celeter/SourceGo@main/{file}'
             print(url)
             #res = requests.get(url).text
             url = f'https://purge.jsdelivr.net/gh/Celeter/SourceGo@main/{file}'
