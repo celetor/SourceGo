fb5b759a80688401bf6dc9b19adbb62089239e4d
Celeter <48249130+Celeter@users.noreply.github.com>
2021-02-28 11:55:53 +0800

    Update purge_cache.py

--- .github/scripts/purge_cache.py
+++ .github/scripts/purge_cache.py
@@ -10,13 +10,13 @@ def get_file_list(folder_dir: str):
         if os.path.isfile(sub_dir):
             file_list.append(d)
         else:
-            folder_list.append(d)
+            folder_list.append(f'book_source/{d}')
     for d in folder_list:
         sub_dir = os.path.join(folder_dir, d)
         f_list = os.listdir(sub_dir)
         for f in f_list:
            if os.path.isfile(os.path.join(sub_dir, f)): 
-                file_list.append(f'{d}/{f}')      
+                file_list.append(f'book_source/{d}/{f}')      
     return file_list
 
 
@@ -30,7 +30,7 @@ if __name__ == '__main__':
             print(file)
             #url = f'https://cdn.jsdelivr.net/gh/Celeter/SourceGo@main/book_source/{file}'
             #res = requests.get(url).text
-            url = f'https://purge.jsdelivr.net/gh/Celeter/SourceGo@main/book_source/{file}'
+            url = f'https://purge.jsdelivr.net/gh/Celeter/SourceGo@main/{file}'
             res = requests.get(url).text
             print(res)
             
